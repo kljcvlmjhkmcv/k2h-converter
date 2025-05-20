d0ff5216602beb8c16893cd8d3dce9a9049bc9bb
kljcvlmjhkmcv <64702702+kljcvlmjhkmcv@users.noreply.github.com>
2025-05-20 21:12:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
+const config = {
+  client_id: "f3fd2e13bd8046e4b801f01e43b4500b",
+  redirect_uri: "https://www.spotmyplaylist.site/login.html",
+  uri: "https://www.spotmyplaylist.site",
+  scope: [
+    "playlist-read-private",
+    "playlist-modify-private",
+    "playlist-modify-public",
+    "user-library-read",
+    "user-library-modify",
+    "user-follow-read",
+    "user-follow-modify"
+  ].join(" ")
+};
+
 let token = null;
 
 function loginWithSpotify() {
